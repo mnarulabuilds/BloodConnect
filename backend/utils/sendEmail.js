f1c810e8a3f95ed742b82deb57502500c4ec78d1
mayank.narula <mayank.narula@apollo247.org>
2026-07-11 17:32:02 +0530

    setup mailer

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+const logger = require("../config/logger");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -11,12 +12,23 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendEmail = async (to, subject, text) => {
-    await transporter.sendMail({
-        from: process.env.EMAIL_USER,
-        to,
-        subject,
-        text,
-    });
+    try {
+        transporter.verify((error, success) => {
+            if (error) {
+                logger.error(error);
+            } else {
+                logger.info("SMTP server is ready --- (success)", success);
+            }
+        });
+        await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            text,
+        });
+    } catch (error) {
+        logger.error(`Sending mail failed with error - `, error)
+    }
 };
 
 module.exports = sendEmail;
